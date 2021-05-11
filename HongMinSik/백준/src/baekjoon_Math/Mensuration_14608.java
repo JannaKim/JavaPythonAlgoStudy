9e4d1f7c7904b38c16825bd5e640bcbae4495c11
sig2nya <starwalk7058@naver.com>
2021-05-11 15:29:34 +0900

    ㅇㅇ

--- HongMinSik/백준/src/baekjoon_Math/Mensuration_14608.java
+++ HongMinSik/백준/src/baekjoon_Math/Mensuration_14608.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Mensuration_14608 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("이클립스 연동 됐냐??!?!?!?!?!?!?!?");
 	}
 
 }
