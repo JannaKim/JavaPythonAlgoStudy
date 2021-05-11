58a95a92af41f8df1724642be8dbf67bb08efc94
sig2nya <starwalk7058@naver.com>
2021-05-11 22:50:28 +0900

    JSP Project

---
+++ HongMinSik/백준/src/baekjoon_Backtracking/NM1_15649.java
@@ -0,0 +1,12 @@
+package baekjoon_Backtracking;
+
+import java.util.Scanner;
+
+public class NM1_15649 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
