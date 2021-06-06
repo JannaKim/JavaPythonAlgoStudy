715d3ace2aaa88dd83d30eb262e0ecede079b3f0
sig2nya <starwalk7058@naver.com>
2021-06-06 15:58:16 +0900

    외 오답임 이게

---
+++ 2021/6_June_2021/210602/숫자정사각형_1051/210606_MinSik_NumSquare_1051.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+arr = []
+
+for i in range(N):
+    arr.append(list(map(int, input())))
+
+len = N - 1 if N <= M else M - 1
+point = N if N < M else M
+area = 0
+
+for i in range(N - len):
+    for j in range(M - len):
+        if(arr[i][j] == arr[i][j + len] and arr[i][j] == arr[i + len][j] and arr[i][j] == arr[i + len][j + len]):
+            area = (len+1) ** 2 if (len+1) ** 2 > area else area
+
+print(area)
