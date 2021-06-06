43c4bbb80d7fd62565364107a07c6865655c79e9
sig2nya <starwalk7058@naver.com>
2021-06-06 16:21:19 +0900

    50% ....

--- 2021/6_June_2021/210602/숫자정사각형_1051/210606_MinSik_NumSquare_1051.py
+++ 2021/6_June_2021/210602/숫자정사각형_1051/210606_MinSik_NumSquare_1051.py
@@ -5,11 +5,14 @@ for i in range(N):
     arr.append(list(map(int, input())))
 
 len = N - 1 if N <= M else M - 1
+print('len :', len)
 area = 0
 
-for i in range(N - len):
-    for j in range(M - len):
-        if(arr[i][j] == arr[i][j + len] and arr[i][j] == arr[i + len][j] and arr[i][j] == arr[i + len][j + len]):
-            area = (len+1) ** 2 if (len+1) ** 2 > area else area
+while(len > 0):
+    for i in range(N - len):
+        for j in range(M - len):
+            if(arr[i][j] == arr[i][j + len] and arr[i][j] == arr[i + len][j] and arr[i][j] == arr[i + len][j + len]):
+                area = (len+1) ** 2 if (len+1) ** 2 > area else area
+    len -= 1
 
 print(area)
