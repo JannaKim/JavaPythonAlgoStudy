5550a52d9ef7373962b125e87c109530ec913826
sig2nya <starwalk7058@naver.com>
2021-06-12 15:30:37 +0900

    Solved 2960 in python

---
+++ HongMinSik/Python/알고리즘/Baekjoon/Math/Eratosthenes_2960.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+check = [False for i in range(N + 1)]
+
+cnt = 0
+
+for i in range(2, N + 1):
+    for j in range(i, N + 1, i):
+        if(check[j] == False):
+            check[j] = True
+            cnt += 1
+        if(cnt == K): 
+            print(j)
+            break
+    if (cnt == K): break
+
+# 조건문에서 j의 증가값을 주어서 i의 배수를 구할수도 있다.
+# 굳이 i * j안해도 된다 이말이야~
