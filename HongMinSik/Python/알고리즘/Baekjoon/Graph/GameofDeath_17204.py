b41808ebf797eaaf0ac9eabcb98c8284065c56ea
sig2nya <starwalk7058@naver.com>
2021-06-14 21:36:21 +0900

    dlrpdhodkseho?

---
+++ HongMinSik/Python/알고리즘/Baekjoon/Graph/GameofDeath_17204.py
@@ -0,0 +1,28 @@
+N, K = map(int, input().split())
+member = [i for i in range(N)]
+adj = [[0]*N for i in range(N)]
+check = [False for i in range(N)]
+result = []
+
+cnt = 0
+
+def DFS(start):
+    if(check[start] == True): 
+        return
+    check[start] = True
+    for i in range(len(adj)):
+        if(check[i] == False and adj[i][start] == 1):
+            result.append(start)
+            DFS(i)
+    return
+
+for i in range(N):
+    member[i] = int(input())
+    adj[i][member[i]] = adj[member[i]][i] = 1
+
+DFS(0)
+print(result)
+if K in result:
+    print(result.index(K))
+else:
+    print(-1)
